1d4c2f4a58851d2c5b9611b3c2ff864e056912ca
HiYasin <hello.mr.faisal@gmail.com>
2025-01-28 19:04:46 +0600

    Testimonial section created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../components/Home/Banner';
+import Testimonial from '../components/Home/Testimonial';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Testimonial></Testimonial>
         </div>
     );
 };
